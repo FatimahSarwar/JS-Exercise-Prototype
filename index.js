69f182f4610b74c6f603c1748e373518d6d77dd9
Fatimah Sarwar <fatimahsarwar@Fatimahs-MBP.fios-router.home>
2021-11-10 15:38:57 -0500

    Finished

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
         + If a plane lands, its `isFlying` property is set to false.
 */
 
+const { throwStatement } = require("@babel/types");
+
 // EXAMPLE SOLUTION CODE:
 function Airplane(name) {
   this.name = name;
@@ -39,14 +41,32 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+}
+Person.prototype.eat= function(edible){
+  if (this.stomach.length < 10){
+    this.stomach.push(edible)
+  }
+}
+Person.prototype.poop = function(){
+  this.stomach = [];
 }
 
+Person.prototype.toString=function(){
+  return `${this.name}, ${this.age}`
+}
 
-
-
-
+const kyler = new Person ('Kyler', 25)
+const Ben = new Person ('Ben', 28)
+console.log(kyler.toString());
+console.log(Ben.toString());
+Ben.eat('Pizza')
+console.log(Ben.stomach)
+Ben.poop();
+console.log(Ben.stomach)
 
 
 /*
@@ -63,10 +83,15 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model , milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
-
+Car.prototype.fill= function(gallons){
+   this.tank = gallons + this.tank
+}
 
 /*
   TASK 3
@@ -75,18 +100,24 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
+function Baby(name, age, favoriteToy) {
+ Person.call(this, name,age)
+ this.favoriteToy = favoriteToy;
 }
-
-
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`
+}
+// const ali = new Baby ('Ali' , 2, 'Annoying Orange')
+// console.log(ali)
+// console.log(ali.play());
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. No binding- bad practice!
+  2. new binding- making a variable and adding new before the constructor function to add more objects.
+  3. Implicit binding. Dot lft to the function.
+  4. Explicit binding : this is added while the objects are being written directly under the function. 
 */
 
 
